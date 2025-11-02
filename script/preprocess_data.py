ed320547314f2f968c1e59daaed501a10a81ac97
Suhani <148181946+suhanasuffer@users.noreply.github.com>
2025-11-03 02:41:03 +0530

    Update preprocess_data.py

--- script/preprocess_data.py
+++ script/preprocess_data.py
@@ -7,7 +7,7 @@ processed_dir = Path(params["data"]["processed_dir"])
 raw_dir = Path(params["data"]["raw_dir"])
 processed_dir.mkdir(parents=True, exist_ok=True)
 
-print(f"Copying raw data from {raw_dir} to {processed_dir} ...")
+#print(f"Copying raw data from {raw_dir} to {processed_dir} ...")
 if processed_dir.exists():
     shutil.rmtree(processed_dir)
 shutil.copytree(raw_dir, processed_dir)
